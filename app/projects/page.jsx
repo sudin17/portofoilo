4649857e71580d981d38b480605bf94541f7d03a
sudin17 <sudin17>
2024-10-20 01:52:59 +0800

    project page

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,8 +1,144 @@
-import react from "react";
+'use client';
+import React, { useState } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import ProjectCard from "@/components/ProjectCard";
+
+const projectData = [
+    {
+        image: '/work/3.png',
+        category: 'wordpress',
+        name: 'Nexa Website',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus reprehenderit quae eaque?',
+        link: '/',
+        github: '/',
+    },
+    {
+        image: '/work/4.png',
+        category: 'react js',
+        name: 'Nexa Website',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus reprehenderit quae eaque?',
+        link: '/',
+        github: '/',
+    },
+    {
+        image: '/work/3.png',
+        category: 'react js',
+        name: 'Nexa Website',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus reprehenderit quae eaque?',
+        link: '/',
+        github: '/',
+    },
+    {
+        image: '/work/1.png',
+        category: 'react js',
+        name: 'Nexa Website',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus reprehenderit quae eaque?',
+        link: '/',
+        github: '/',
+    },
+    {
+        image: '/work/2.png',
+        category: 'react js',
+        name: 'Nexa Website',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus reprehenderit quae eaque?',
+        link: '/',
+        github: '/',
+    },
+    {
+        image: '/work/4.png',
+        category: 'react js',
+        name: 'Nexa Website',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus reprehenderit quae eaque?',
+        link: '/',
+        github: '/',
+    },
+    {
+        image: '/work/1.png',
+        category: 'react js',
+        name: 'Nexa Website',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus reprehenderit quae eaque?',
+        link: '/',
+        github: '/',
+    },
+    {
+        image: '/work/3.png',
+        category: 'react js',
+        name: 'Nexa Website',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus reprehenderit quae eaque?',
+        link: '/',
+        github: '/',
+    },
+    {
+        image: '/work/2.png',
+        category: 'react js',
+        name: 'Nexa Website',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus reprehenderit quae eaque?',
+        link: '/',
+        github: '/',
+    },
+    {
+        image: '/work/1.png',
+        category: 'react js',
+        name: 'Nexa Website',
+        description: 'Lorem, ipsum ',
+        link: '/',
+        github: '/',
+    },
+    {
+        image: '/work/4.png',
+        category: 'react js',
+        name: 'Nexa Website',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus reprehenderit quae eaque?',
+        link: '/',
+        github: '/',
+    },
+];
+
+const uniqueCategories = [
+    'all projects',
+    ...new Set(projectData.map((item) => item.category)),
+];
 
 const Projects = () => {
-    return <div> My Projects 
-    </div>;
+    const [categories, setCategories] = useState(uniqueCategories);
+    const [category, setCategory] = useState('all projects')
+    const filteredProjects = projectData.filter((project) => {
+        return category === 'all projects'
+            ? project
+            : project.category === category;
+    });
+
+    return (
+        <section className="min-h-screen pt-12">
+            <div className="container mx-auto">
+                <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">My Project</h2>
+                <Tabs defaultValue={category} className="mb-24 xl:mb-48">
+                    <TabsList className='w-full grid h-full md:grid-cols-3 lg:max-w-[640px] mb-12 mx-auto md:border dark:border:none'>
+                        {categories.map((category, index) => {
+                            return (
+                                <TabsTrigger 
+                                    onClick={() => setCategory(category)}
+                                    value={category}
+                                    key={index}
+                                    className='capitalize w-[162px] md:w-auto'
+                                    >{category}
+                                </TabsTrigger>
+                            );
+                        })}
+                    </TabsList>
+                    <div className="text-lg xl:mt-8 grid grid grid-cols-1 lg:grid-cols-3 gap-4">
+                        {filteredProjects.map((project, index) => {
+                            return (
+                                <TabsContent value={category} key={index}>
+                                    <ProjectCard project={project}/>
+                                </TabsContent>
+                            );                
+                        })}
+                    </div>
+                </Tabs>
+            </div>
+        </section>
+    );
 };
 
 export default Projects;
